1a0d2e2adf9c8fda176f1f0c712ca3e62428bb4d
shemhazai <andre.marques@fatec.sp.gov.br>
2017-02-27 11:01:19 -0300

    Upping one version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='crypt-en',
-      version='1.9',
+      version='1.9.2',
       description="""
 Shemhazai`s cryptography utility for cryptography.\n
     This program has the following features:
