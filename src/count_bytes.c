5be42f8b373bd2b12b3019417540515180273012
zero-cool <andre.marques@fatec.sp.gov.br>
2017-03-11 14:54:33 -0300

    Fixing how count bytes were counted.

--- src/count_bytes.c
+++ src/count_bytes.c
@@ -7,44 +7,38 @@
 #include <math.h>
 
 #define BUFSIZE 256
+#ifndef DEBUG
+#define DEBUG 0
+#endif
 
 char* count_bytes(long long size)
 {
+    if(DEBUG == 1) {
+        fprintf(stdout, "[DEBUG] long long size: %llu\n", size);
+    }
 
     char* buffer = malloc(sizeof(char) * BUFSIZE);
-    long tera, giga, mega, kilo;
+    long long tera, giga, mega, kilo;
 
-    tera = (long)pow(2, 40);
-    giga = (long)pow(2, 30);
-    mega = (long)pow(2, 20);
-    kilo = (long)pow(2, 10);
+    tera = (long long)pow(2, 40);
+    giga = (long long)pow(2, 30);
+    mega = (long long)pow(2, 20);
+    kilo = (long long)pow(2, 10);
 
     if(size >= tera) {
-        double d = pow(10, 5);
-        double q = size / tera;
-        double y = (size % tera) /d;
-        q += y;
+        double q = size / (double)tera;
         sprintf(buffer, "%2.2f TiB", q);
     } else
     if(size >= giga) {
-        double d = pow(10, 4);
-        double q = size / giga;
-        double y = (size % giga) / d;
-        q += y;
+        double q = size / (double)giga;
         sprintf(buffer, "%2.2f GiB", q);
     } else
     if(size >= mega) {
-        double d = pow(10, 3);
-        double q = size / mega;
-        double y = (size % mega) / d;
-        q += y;
+        double q = size / (double)mega;
         sprintf(buffer, "%2.2f MiB", q);
     } else
     if(size >= kilo) {
-        double d = pow(10, 2);
-        double q = size / kilo;
-        double y = (size % kilo) / d;
-        q += y;
+        double q = size / (double)kilo;
         sprintf(buffer, "%2.2f KiB", q);
     } else {
         sprintf(buffer, "%2.0f Bytes", (double)size);
